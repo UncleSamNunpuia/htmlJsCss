97b55902f47118f8fce9a1387d9f0101d5f0416e
UncleSamNunpuia <70100222+UncleSamNunpuia@users.noreply.github.com>
2026-05-31 02:58:47 +0530

    update js1.js

--- js1.js
+++ js1.js
@@ -1,14 +1,26 @@
-// js1.js
-
 const WEB_APP_URL =
-"https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSXMYzbQ8uriM-VRyEBfmiYmKtZbEM6EL8Jje0VJxVW2x4OQO6PSpa7Bl-ETzzDn9O3v36ElWl67FYnz5D-tgeP4XWjh01Wkt-Mr-sXZ4F7sOCLT1A5E2apV6o_4N0Vn42YVcoXKjO-1t6rzFpVaCYsxtPi5K4ls7o32iozsruO1t2YJ5GxtIqdL9ncSMWEwWkXS-lySRLNRP1P2lWxixPlqrufjvf40CFqr0kxa1TuU0JRVqfUecg5SPnykAE1uNTJHbohIKcvPSM3jUVJB068v_uD4Q&lib=MgNIQUt0RtEgtWhxuTOFKV_Q-sqP20Ani";
+"https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
 
 const receiptInput =
 document.getElementById("receiptNo");
 
-receiptInput.addEventListener(
-  "input",
+const searchBtn =
+document.getElementById("searchBtn");
+
+searchBtn.addEventListener(
+  "click",
   fetchReceiptData
+);
+
+receiptInput.addEventListener(
+  "keypress",
+  function(event){
+
+    if(event.key === "Enter"){
+
+      fetchReceiptData();
+    }
+  }
 );
 
 async function fetchReceiptData(){
@@ -20,7 +32,11 @@ async function fetchReceiptData(){
 
   if(receiptNo === ""){
 
-    setStatus("", "");
+    setStatus(
+      "Please enter receipt number",
+      "error"
+    );
+
     return;
   }
 
