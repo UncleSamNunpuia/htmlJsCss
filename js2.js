be5701e28a463270c9aceef0a19de1eb458c6059
UncleSamNunpuia <70100222+UncleSamNunpuia@users.noreply.github.com>
2026-06-09 00:55:21 +0530

    update js2.js

--- js2.js
+++ js2.js
@@ -1,7 +1,10 @@
-window.onload = function(){
+document.addEventListener("DOMContentLoaded", function() {
   alert("hi js2 ");
   function changeColor() {
     document.queryselector(".pc2").innerHTML === "paragrqph changed delue to click";
-  }
-}
+  }  // code
+});
+// window.onload = function(){
+  
+// }
 
